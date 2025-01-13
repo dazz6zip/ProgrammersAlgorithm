7c935823a746b63aa42c145ff429d053bad7fac5
dazz6 <dazz6@kakao.com>
2025-01-13 17:35:38 +0900

    1번 출력 by dazz6

---
+++ src/main/java/org/pack/jan_1/PCCE/Lv0_출력/dazz6.java
@@ -0,0 +1,13 @@
+package org.pack.jan_1.PCCE.Lv0_출력;
+
+public class dazz6 {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
