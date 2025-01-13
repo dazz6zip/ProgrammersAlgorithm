33b867e28bf493d385893330c6aee25db8316c33
dazz6 <dazz6@kakao.com>
2025-01-13 16:01:44 +0900

    package

---
+++ src/main/java/org/pack/jan_1/PCCE/Lv0_가습기/chooi.java
@@ -0,0 +1,4 @@
+package org.pack.jan_1.PCCE.Lv0_가습기;
+
+public class chooi {
+}
---
+++ src/main/java/org/pack/jan_1/PCCE/Lv0_가습기/dazz6.java
@@ -0,0 +1,7 @@
+package org.pack.jan_1.PCCE.Lv0_가습기;
+
+public class dazz6 {
+    public static void main(String[] args) {
+        System.out.print("dazz6");
+    }
+}
