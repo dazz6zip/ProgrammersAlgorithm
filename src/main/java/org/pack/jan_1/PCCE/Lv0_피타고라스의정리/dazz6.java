ac75c50e114c54b9ff4f9cee73bfce5f3b71a35e
dazz6 <dazz6@kakao.com>
2025-01-13 17:43:14 +0900

    2번 피타고라스의 정리 by dazz6

---
+++ src/main/java/org/pack/jan_1/PCCE/Lv0_피타고라스의정리/dazz6.java
@@ -0,0 +1,15 @@
+package org.pack.jan_1.PCCE.Lv0_피타고라스의정리;
+
+import java.util.Scanner;
+
+public class dazz6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = (int) (Math.pow(c, 2) - Math.pow(a, 2));
+
+        System.out.println(b_square);
+    }
+}
