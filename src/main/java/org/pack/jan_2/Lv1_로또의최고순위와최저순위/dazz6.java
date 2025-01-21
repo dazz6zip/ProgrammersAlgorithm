b7e27485b744e0facccee980f9429a513a019dc5
dazz6 <dazz6@kakao.com>
2025-01-21 20:30:01 +0900

    로또의 최고 순위와 최저 순위 by dazz6

---
+++ src/main/java/org/pack/jan_2/Lv1_로또의최고순위와최저순위/dazz6.java
@@ -0,0 +1,33 @@
+package org.pack.jan_2.Lv1_로또의최고순위와최저순위;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class dazz6 {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int count = 6;
+        for (int i = 1; i < 7; i++) {
+            map.put(i, count);
+            count--;
+        }
+
+        int rightCount = 0;
+        int unknownCount = 0;
+
+        for (int l : lottos) {
+            if (l != 0) {
+                for (int w : win_nums) {
+                    if (l == w) {
+                        rightCount++;
+                    }
+                }
+            } else {
+                unknownCount++;
+            }
+        }
+
+        return new int[]{map.get(Math.max(1, rightCount + unknownCount)), map.get(Math.max(1, rightCount))};
+    }
+}
