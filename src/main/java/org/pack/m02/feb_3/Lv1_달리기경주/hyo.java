aee1f1a8bc293637ef397c68c6bb996880b11502
hyo <hyoseonee0729@naver.com>
2025-02-27 01:02:48 +0900

    는 시간 초과이다 배열 순회 < Hashmap 명 심 해

--- src/main/java/org/pack/m02/feb_3/Lv1_달리기경주/hyo.java
+++ src/main/java/org/pack/m02/feb_3/Lv1_달리기경주/hyo.java
@@ -1,15 +1,24 @@
 package org.pack.m02.feb_3.Lv1_달리기경주;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class hyo {
     public String[] solution(String[] players, String[] callings) {
-        for(String calling : callings){
-            for(int i=0; i<players.length; i++){
-                if(players[i].equals(calling)){
-                    String temp = players[i-1];
-                    players[i-1] = players[i];
-                    players[i] = temp;
-                    break;
-                }
+        Map<String, Integer> playerIndex = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            playerIndex.put(players[i], i);
+        }
+
+        for (String calling : callings) {
+            int idx = playerIndex.get(calling);
+            if (idx > 0) { // 맨 앞이 아니라면 교체 가능
+                String temp = players[idx - 1];
+                players[idx - 1] = players[idx];
+                players[idx] = temp;
+
+                playerIndex.put(calling, idx - 1);
+                playerIndex.put(temp, idx);
             }
         }
         return players;
