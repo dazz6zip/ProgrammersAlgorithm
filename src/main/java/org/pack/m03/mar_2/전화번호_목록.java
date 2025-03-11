6f4e0a24a0330df51c65e1a7c8a77bcc3fe6662e
dazz6zip <dazz6@kakao.com>
2025-03-11 10:25:04 +0900

    해시 > 전화번호 목록

--- src/main/java/org/pack/m03/mar_2/전화번호_목록.java
+++ src/main/java/org/pack/m03/mar_2/전화번호_목록.java
@@ -1,4 +1,17 @@
 package org.pack.m03.mar_2;
 
+import java.util.*;
+
 public class 전화번호_목록 {
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
