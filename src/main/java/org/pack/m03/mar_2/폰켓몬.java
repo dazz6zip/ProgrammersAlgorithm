ac1690fb749033ebaed9b9965aff1f2bb80b095d
dazz6zip <dazz6@kakao.com>
2025-03-11 09:14:33 +0900

    해시 > 폰켓몬

--- src/main/java/org/pack/m03/mar_2/폰켓몬.java
+++ src/main/java/org/pack/m03/mar_2/폰켓몬.java
@@ -1,4 +1,15 @@
 package org.pack.m03.mar_2;
 
+import java.util.*;
+
 public class 폰켓몬 {
+    public static int solution(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        return Math.min(map.size(), nums.length / 2);
+    }
 }
