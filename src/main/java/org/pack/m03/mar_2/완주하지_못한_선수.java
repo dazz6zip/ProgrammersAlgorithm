53e13f0644d62bc96634a43b63fc93f368ff85a7
dazz6zip <dazz6@kakao.com>
2025-03-11 09:32:34 +0900

    해시 > 완주하지 못한 선수 (수정)

--- src/main/java/org/pack/m03/mar_2/완주하지_못한_선수.java
+++ src/main/java/org/pack/m03/mar_2/완주하지_못한_선수.java
@@ -3,12 +3,6 @@ package org.pack.m03.mar_2;
 import java.util.*;
 
 public class 완주하지_못한_선수 {
-    public static void main(String[] args) {
-        System.out.println("leo : " + solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
-        System.out.println("vinko : " + solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
-        System.out.println("mislav : " + solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
-    }
-
     public static String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         for (String p : participant) {
