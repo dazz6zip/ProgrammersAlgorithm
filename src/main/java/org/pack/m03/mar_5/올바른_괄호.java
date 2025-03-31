c782e116e8c0be6abaee98d869ef029b51fef46a
dazz6 <dazz6@kakao.com>
2025-03-31 14:18:33 +0900

    스택/큐 > 올바른 괄호

--- src/main/java/org/pack/m03/mar_5/올바른_괄호.java
+++ src/main/java/org/pack/m03/mar_5/올바른_괄호.java
@@ -1,4 +1,29 @@
 package org.pack.m03.mar_5;
 
+import java.util.*;
+
 public class 올바른_괄호 {
+    public static void main(String[] args) {
+        System.out.println("true : " + solution("()()"));
+        System.out.println("true : " + solution("(())()"));
+        System.out.println("false : " + solution(")()("));
+        System.out.println("false : " + solution("(()("));
+    }
+
+    public static boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                st.push(c);
+            } else if (c == ')') {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.isEmpty();
+    }
 }
