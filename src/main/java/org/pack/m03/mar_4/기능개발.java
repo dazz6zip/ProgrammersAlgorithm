1cf6cf53c41b87ac08c2d05aceaf37917f7fa355
dazz6 <dazz6@kakao.com>
2025-03-31 14:07:33 +0900

    스택/큐 > 기능개발 (수정)

--- src/main/java/org/pack/m03/mar_4/기능개발.java
+++ src/main/java/org/pack/m03/mar_4/기능개발.java
@@ -3,11 +3,6 @@ package org.pack.m03.mar_4;
 import java.util.*;
 
 public class 기능개발 {
-    public static void main(String[] args) {
-        System.out.println("2, 1 : " + Arrays.toString(solution(new int[]{93, 30, 55}, new int[]{1, 30, 5})));
-        System.out.println("1, 3, 2 : " + Arrays.toString(solution(new int[]{95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1})));
-    }
-
     public static int[] solution(int[] progresses, int[] speeds) {
         List<Integer> list = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
