98a1552196d0b1fa020b9d770df2996df88e2ffe
dazz6 <dazz6@kakao.com>
2025-04-07 15:40:43 +0900

    스택/큐 > 주식가격

--- src/main/java/org/pack/m04/apr_1/주식가격.java
+++ src/main/java/org/pack/m04/apr_1/주식가격.java
@@ -1,4 +1,29 @@
 package org.pack.m04.apr_1;
 
+import java.util.*;
+
 public class 주식가격 {
+    public static void main(String[] args) {
+        System.out.println("4, 3, 1, 1, 0 : " + Arrays.toString(solution(new int[]{1, 2, 3, 2, 3})));
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int t = stack.pop();
+                answer[t] = i - t;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int t = stack.pop();
+            answer[t] = prices.length - 1 - t;
+        }
+
+        return answer;
+    }
 }
