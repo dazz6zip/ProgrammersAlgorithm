51690713f74026c2ba50617108e488e0c39fa8a3
dazz6 <dazz6@kakao.com>
2025-04-14 19:15:44 +0900

    힙 > 더 맵게

--- src/main/java/org/pack/m04/apr_2/더_맵게.java
+++ src/main/java/org/pack/m04/apr_2/더_맵게.java
@@ -1,4 +1,32 @@
 package org.pack.m04.apr_2;
 
+import java.util.*;
+
 public class 더_맵게 {
+    public static void main(String[] args) {
+        System.out.println("2 : " + solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
+    }
+
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int s : scoville) {
+            queue.offer(s);
+        }
+
+        while (queue.size() >= 2 && queue.peek() < K) {
+            int one = queue.poll();
+            int two = queue.poll();
+
+            queue.offer(one + (two * 2));
+            answer++;
+        }
+
+        if (queue.peek() < K) {
+            return -1;
+        }
+
+        return answer;
+    }
 }
