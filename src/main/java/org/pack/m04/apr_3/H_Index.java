591538b323c99e509003865468cf43aed2dee5b5
dazz6 <dazz6@kakao.com>
2025-04-29 20:16:32 +0900

    정렬 > H Index

--- src/main/java/org/pack/m04/apr_3/H_Index.java
+++ src/main/java/org/pack/m04/apr_3/H_Index.java
@@ -1,4 +1,29 @@
 package org.pack.m04.apr_3;
 
+import java.util.*;
+
 public class H_Index {
+    public static void main(String[] args) {
+        System.out.println("3 : " + solution(new int[]{3, 0, 6, 1, 5}));
+    }
+
+    public static int solution(int[] citations) {
+        List<Integer> list = new ArrayList<>();
+        for (int citation : citations) {
+            list.add(citation);
+        }
+
+        list.sort(Collections.reverseOrder());
+
+        int h = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) >= i + 1) {
+                h = i + 1;
+            } else {
+                break;
+            }
+        }
+
+        return h;
+    }
 }
