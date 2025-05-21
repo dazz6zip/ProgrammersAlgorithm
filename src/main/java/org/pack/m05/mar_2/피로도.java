c5eba3b111246dd702f5348bfbd0e9757f287627
dazz6 <dazz6@kakao.com>
2025-05-21 14:14:15 +0900

    완전탐색 > 피로도

--- src/main/java/org/pack/m05/mar_2/피로도.java
+++ src/main/java/org/pack/m05/mar_2/피로도.java
@@ -1,4 +1,27 @@
 package org.pack.m05.mar_2;
 
 public class 피로도 {
+    public static void main(String[] args) {
+        System.out.println("3 : " + solution(80, new int[][]{{80, 20}, {50, 40}, {30, 10}}));
+    }
+
+    public static int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        return dfs(k, dungeons, visited, 0);
+    }
+
+    private static int dfs(int k, int[][] dungeons, boolean[] visited, int count) {
+        int max = count;
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                int result = dfs(k - dungeons[i][1], dungeons, visited, count + 1);
+                max = Math.max(max, result);
+                visited[i] = false;
+            }
+        }
+
+        return max;
+    }
 }
